80dc0d662ba243d52a2b73d0961ac8b3638cf7f6
Muciej <kejdzuuu@gmail.com>
2020-11-09 00:36:46 +0100

    grub-mkrescue does not create proper isos; changed scripts to use binary kernel

--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
-. ./iso.sh
+#. ./iso.sh
 
-qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom macos.iso
+#qemu-system-$(./target-triplet-to-arch.sh $HOST) -cdrom macos.iso
+. ./config.sh
+
+qemu-system-$(./target-triplet-to-arch.sh $HOST) -kernel kernel/macos.kernel
